adb7a2d004b3bda5c6069427948d6d0c6044b7a0
Jon Leighton <hello@jonleighton.name>
2022-09-12 17:59:22 +1000

    Enforce HTTPS

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
+var enforce = require("express-sslify");
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL = process.env.NEW_BASE_URL || "http://example.com";
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
-app.get('*', function(request, response) {
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.get("*", function(request, response) {
   response.redirect(redirectStatus, newBaseURL + request.url);
 });
 
